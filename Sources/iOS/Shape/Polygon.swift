77cadcb31d818ac59675a30a76b656a888bcdbd9
Khoa Pham <onmyway133@gmail.com>
2016-11-06 23:23:32 +0100

    Draw polylines

--- Sources/iOS/Shape/Polygon.swift
+++ Sources/iOS/Shape/Polygon.swift
@@ -4,16 +4,7 @@ class Polygon: Shape {
   var points: [CGPoint] = []
   
   required init(attributes: JSONDictionary) {
-    let pairs: [String] = attributes.string(key: "points")?.components(separatedBy: " ") ?? []
-    self.points = pairs.flatMap { pair in
-      let components = pair.components(separatedBy: ",")
-      if components.count == 2 {
-        return CGPoint(x: CGFloat(string: components[0]),
-                       y: CGFloat(string: components[1]))
-      } else {
-        return nil
-      }
-    }
+    self.points = Polyline.parse(string: attributes.string(key: "points"))
     
     super.init(attributes: attributes)
     
--- Sources/iOS/Shape/Polyline.swift
+++ Sources/iOS/Shape/Polyline.swift
@@ -4,6 +4,30 @@ class Polyline: Shape {
   let points: [CGPoint]
   
   required init(attributes: JSONDictionary) {
-    fatalError()
+    self.points = Polyline.parse(string: attributes.string(key: "points"))
+    
+    super.init(attributes: attributes)
+    
+    self.path = UIBezierPath()
+    if let first = self.points.first {
+      self.path?.move(to: first)
+      
+      self.points.dropFirst().forEach {
+        self.path?.addLine(to: $0)
+      }
+    }
+  }
+  
+  static func parse(string: String?) -> [CGPoint] {
+    let pairs: [String] = string?.components(separatedBy: " ") ?? []
+    return pairs.flatMap { pair in
+      let components = pair.components(separatedBy: ",")
+      if components.count == 2 {
+        return CGPoint(x: CGFloat(string: components[0]),
+                       y: CGFloat(string: components[1]))
+      } else {
+        return nil
+      }
+    }
   }
 }
