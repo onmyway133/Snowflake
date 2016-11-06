c3a4c494323a99104cbeb768a381c1cda262e896
Khoa Pham <onmyway133@gmail.com>
2016-11-06 21:41:11 +0100

    Parse rgb strings

--- Sources/iOS/Utils/Color.swift
+++ Sources/iOS/Utils/Color.swift
@@ -5,13 +5,13 @@ struct Color {
   static func color(name: String) -> UIColor {
     if name.hasPrefix("#") {
       return color(hexString: name)
-    }
-    
-    if let hexString = mapping[name] {
+    } else if name.hasPrefix("rgb") {
+      return color(rgbString: name)
+    } else if let hexString = mapping[name] {
       return color(hexString: hexString)
+    } else {
+      return UIColor.white
     }
-    
-    return UIColor.white
   }
   
   // https://github.com/hyperoslo/Hue/blob/master/Source/iOS/UIColor%2BHue.swift
@@ -35,6 +35,20 @@ struct Color {
       red:   CGFloat((Int(hex, radix: 16)! >> 16) & 0xFF) / 255.0,
       green: CGFloat((Int(hex, radix: 16)! >> 8) & 0xFF) / 255.0,
       blue:  CGFloat((Int(hex, radix: 16)!) & 0xFF) / 255.0, alpha: 1.0)
+  }
+  
+  static func color(rgbString: String) -> UIColor {
+    let string = rgbString.replacingOccurrences(of: "rgb(", with: "")
+      .replacingOccurrences(of: ")", with: "")
+    let components: [CGFloat] = string.components(separatedBy: ",").map {
+      return CGFloat(string: $0)
+    }
+    
+    if components.count == 3 {
+      return UIColor(red: components[0], green: components[1], blue: components[2], alpha: 1)
+    } else {
+      return UIColor.white
+    }
   }
   
   // http://www.december.com/html/spec/colorsvghex.html
--- Sources/iOS/Utils/Extensions.swift
+++ Sources/iOS/Utils/Extensions.swift
@@ -4,9 +4,8 @@ extension Dictionary {
   
   func number(key: String) -> CGFloat? {
     if let key = key as? Key,
-      let value = self[key] as? String,
-      let number = Double(value) {
-      return CGFloat(number)
+      let value = self[key] as? String {
+      return CGFloat(string: value)
     }
     
     return nil
@@ -26,5 +25,13 @@ extension Dictionary {
     }
     
     return nil
+  }
+}
+
+extension CGFloat {
+  
+  init(string: String) {
+    let value = Double(string) ?? 0
+    self = CGFloat(value)
   }
 }
