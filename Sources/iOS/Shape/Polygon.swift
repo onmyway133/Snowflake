ceb5dd962afc6c410bae78f6be4e377af04d3afe
Khoa Pham <onmyway133@gmail.com>
2016-11-06 23:12:02 +0100

    Draw polygon

--- Sources/iOS/Shape/Polygon.swift
+++ Sources/iOS/Shape/Polygon.swift
@@ -1,9 +1,31 @@
 import UIKit
 
 class Polygon: Shape {
-  let points: [CGPoint]
+  var points: [CGPoint] = []
   
   required init(attributes: JSONDictionary) {
-    fatalError()
+    let pairs: [String] = attributes.string(key: "points")?.components(separatedBy: " ") ?? []
+    self.points = pairs.flatMap { pair in
+      let components = pair.components(separatedBy: ",")
+      if components.count == 2 {
+        return CGPoint(x: CGFloat(string: components[0]),
+                       y: CGFloat(string: components[1]))
+      } else {
+        return nil
+      }
+    }
+    
+    super.init(attributes: attributes)
+    
+    self.path = UIBezierPath()
+    if let first = self.points.first {
+      self.path?.move(to: first)
+      
+      self.points.dropFirst().forEach {
+        self.path?.addLine(to: $0)
+      }
+      
+      self.path?.close()
+    }
   }
 }
