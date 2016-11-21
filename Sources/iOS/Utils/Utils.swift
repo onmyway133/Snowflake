d3d4818b5841fd8103cc92619c12ae6ee70cf295
Khoa Pham <onmyway133@gmail.com>
2016-11-21 23:10:44 +0100

    Check isLowercase

--- Sources/iOS/Utils/Utils.swift
+++ Sources/iOS/Utils/Utils.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct Utils {
+public struct Utils {
 
   static func number(string: String) -> CGFloat {
     let value = Double(string) ?? 0
@@ -10,6 +10,16 @@ struct Utils {
   static func numbers(string: String) -> [CGFloat] {
     return string.components(separatedBy: " ").flatMap {
       return number(string: $0)
+    }
+  }
+
+  public static func isLowercase(string: String) -> Bool {
+    let set = CharacterSet.lowercaseLetters
+
+    if let scala = UnicodeScalar(string) {
+      return set.contains(scala)
+    } else {
+      return false
     }
   }
 }
