c8eb0f64752f4db14f2c3594ed37f50912d9f467
Khoa Pham <onmyway133@gmail.com>
2016-11-22 23:35:48 +0100

    Add svg files for arc path

--- Sources/iOS/Command/EllipticalArcCommand.swift
+++ Sources/iOS/Command/EllipticalArcCommand.swift
@@ -1,5 +1,25 @@
 import Foundation
 
 class EllipticalArcCommand: Command {
-    
+  var controlPoint: CGPoint = .zero
+  var endPoint: CGPoint = .zero
+
+  required init(string: String, kind: Kind) {
+    super.init(string: string, kind: kind)
+
+    let numbers = Utils.numbers(string: string)
+    if numbers.count == 4 {
+      controlPoint = CGPoint(x: numbers[0], y: numbers[1])
+      endPoint = CGPoint(x: numbers[2], y: numbers[3])
+    }
+  }
+
+  override func act(path: UIBezierPath) {
+    switch kind {
+    case .absolute:
+      break
+    case .relative:
+      break
+    }
+  }
 }
