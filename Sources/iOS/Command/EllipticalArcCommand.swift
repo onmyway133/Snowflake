45d571ae26791e74ab7aec5971175eed9bab5470
Khoa Pham <onmyway133@gmail.com>
2016-11-23 23:26:39 +0100

    Parse arc values

--- Sources/iOS/Command/EllipticalArcCommand.swift
+++ Sources/iOS/Command/EllipticalArcCommand.swift
@@ -1,16 +1,22 @@
 import Foundation
 
 class EllipticalArcCommand: Command {
-  var controlPoint: CGPoint = .zero
+  var radius: CGPoint = .zero
+  var rotation: CGFloat = 0
+  var largeArcFlag: Bool = false
+  var sweepFlag: Bool = false
   var endPoint: CGPoint = .zero
 
   required init(string: String, kind: Kind) {
     super.init(string: string, kind: kind)
 
     let numbers = Utils.numbers(string: string)
-    if numbers.count == 4 {
-      controlPoint = CGPoint(x: numbers[0], y: numbers[1])
-      endPoint = CGPoint(x: numbers[2], y: numbers[3])
+    if numbers.count == 7 {
+      radius = CGPoint(x: numbers[0], y: numbers[1])
+      rotation = numbers[2]
+      largeArcFlag = numbers[3] > 0 ? true : false
+      sweepFlag = numbers[4] > 0 ? true : false
+      endPoint = CGPoint(x: numbers[5], y: numbers[6])
     }
   }
 
