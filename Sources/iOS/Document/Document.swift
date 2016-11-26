ae19e6d93e5842737f0127dba3113a1f76faed5e
Khoa Pham <onmyway133@gmail.com>
2016-11-26 14:04:57 +0100

    Not use XPath for now

--- Sources/iOS/Document/Document.swift
+++ Sources/iOS/Document/Document.swift
@@ -8,15 +8,9 @@ public class Document {
   public let svg: SVG
   
   public init?(data: Data) {
-    guard let document = try? Reindeer.Document(data: data) else { return nil }
+    guard let document = try? Reindeer.Document(data: data),
+      document.rootElement.name == "svg" else { return nil }
 
-    // http://stackoverflow.com/questions/3135175/libxml2-error-with-namespaces-and-xpath
-    let XPath: String = document.hasNamespace ? "//new:svg" : "//svg"
-    let namespace: String? = document.hasNamespace ? "new" : nil
-    guard let svg: SVG = document.rootElement.elements(XPath: XPath, namespace: namespace).map({
-      return SVG(element: $0)
-    }).first else { return nil }
-
-    self.svg = svg
+    self.svg = SVG(element: document.rootElement)
   }
 }
