3deb978a3a7072ed80d6a4eb4abcfebe4cf54f22
Khoa Pham <onmyway133@gmail.com>
2016-11-29 23:22:22 +0100

    Add storyboard

--- Example/SnowflakeDemo/SnowflakeDemo/Sources/AppDelegate.swift
+++ Example/SnowflakeDemo/SnowflakeDemo/Sources/AppDelegate.swift
@@ -6,20 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  lazy var navigationController: UINavigationController = { [unowned self] in
-    let controller = UINavigationController(rootViewController: self.viewController)
-    return controller
-    }()
-
-  lazy var viewController: ListController = {
-    let controller = ListController()
-    return controller
-    }()
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = navigationController
-    window?.makeKeyAndVisible()
 
     return true
   }
