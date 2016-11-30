b9f0a19b5cc8c2fc69cde3d16c091b57a85ffcc1
Khoa Pham <onmyway133@gmail.com>
2016-11-30 22:33:21 +0100

    Parse UIImage from base 64 encoded string

--- Sources/iOS/Item/Image.swift
+++ Sources/iOS/Item/Image.swift
@@ -3,13 +3,37 @@ import Reindeer
 
 public class Image: Item {
 
+  let rect: CGRect
+  let image: UIImage?
+
   public required init(attributes: JSONDictionary) {
+    self.rect = CGRect(x: attributes.number(key: "x") ?? 0,
+                       y: attributes.number(key: "y") ?? 0,
+                       width: attributes.number(key: "width") ?? 0,
+                       height: attributes.number(key: "height") ?? 0)
+
+    self.image = Image.parse(string: attributes.string(key: "href") ?? "")
     super.init(attributes: attributes)
   }
 
   public func layer() -> CALayer {
     let layer = CALayer()
+    layer.contents = image?.cgImage
 
     return layer
+  }
+
+  // MARK: - Helper
+
+  static func parse(string: String) -> UIImage? {
+    guard !string.isEmpty else { return nil }
+
+    let header = "data:image/png;base64,"
+    guard let range = string.range(of: header) else { return nil }
+
+    let substring = string.substring(from: range.upperBound)
+    guard let data = Data(base64Encoded: string, options: .ignoreUnknownCharacters) else { return nil }
+
+    return UIImage(data: data)
   }
 }
