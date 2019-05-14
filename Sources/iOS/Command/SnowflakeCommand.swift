3f8197c5711fea76adf178fbc057e6046c5fe0e7
Khoa Pham <onmyway133@gmail.com>
2019-05-14 16:45:47 +0200

    Add svg

--- Sources/iOS/Command/SnowflakeCommand.swift
+++ Sources/iOS/Command/SnowflakeCommand.swift
@@ -1,13 +1,51 @@
 //
-//  Command.swift
-//  SnowflakeCommand
+//  SnowflakeCommand.swift
+//  Command
 //
+//  https://github.com/onmyway133/Snowflake
 //  Created by khoa on 14/05/2019.
 //
 
 import CoreGraphics
 import Foundation
 import UIKit
+
+public extension UIBezierPath {
+    static func from(svgPath: String) -> UIBezierPath {
+        let path = UIBezierPath()
+
+        let set = CharacterSet(charactersIn: Command.letters)
+        let scanner = Scanner(string: string)
+
+        var initial: NSString? = ""
+        var numbers: NSString? = ""
+        var commands: [Command] = []
+
+        repeat {
+            scanner.scanCharacters(from: set, into: &initial)
+            guard initial!.length > 0 else { break }
+
+            scanner.scanUpToCharacters(from: set, into: &numbers)
+            guard numbers!.length > 0 else { break }
+
+            if let command = Command.make(initial: String(initial!), string: String(numbers!)) {
+                commands.append(command)
+            }
+
+            if scanner.scanLocation == string.count {
+                break
+            }
+        } while initial!.length > 0
+
+        commands.enumerated().forEach { (index, command) in
+            var previousCommand: Command? = index > 0 ? self.commands[index-1] : nil
+            command.act(path: path, previousCommand: previousCommand)
+        }
+
+
+        return path
+    }
+}
 
 public class Command {
 
